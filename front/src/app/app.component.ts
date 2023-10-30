b8b7dfa2a1bb9a3e436aabb9a1d6c08e5e352ba5
dimitraye <dimitraye94@gmail.com>
2023-10-30 19:39:01 +0100

    front : views and routes

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { AuthService } from './features/auth/services/auth.service';
+import { User } from './interfaces/user.interface';
+import { SessionService } from './services/session.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'front';
+
+export class AppComponent implements OnInit {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private sessionService: SessionService) {
+  }
+
+  public ngOnInit(): void {
+    this.autoLog();
+  }
+
+  public $isLogged(): Observable<boolean> {
+    //return this.sessionService.$isLogged();
+    return of(false);
+  }
+
+  public logout(): void {
+    this.sessionService.logOut();
+    this.router.navigate([''])
+  }
+
+  public autoLog(): void {
+    this.authService.me().subscribe(
+      (user: User) => {
+        this.sessionService.logIn(user);
+      },
+      (_) => {
+        this.sessionService.logOut();
+      }
+    )
+  }
 }
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { MeComponent } from './components/me/me.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+const materialModule = [
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatToolbarModule,
+]
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent, MeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
+    HttpClientModule,
+    ...materialModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
